16fe08eefb0aad39f68a4ab220aee39b63d547be
23060107-Gong Kai Chen <1106029170@qq.com>
2024-04-26 17:19:45 +0800

    添加 telescope 模糊查找器

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
-
+vim.g.mapleader=" "
 
 -- 添加包管理器 lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -23,9 +23,24 @@ local plugins = {
 		"catppuccin/nvim",
 		name="catppuccin",
 		priority=1000
+	},
+	-- 安装模糊查找器 telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		tag="0.1.5",
+		dependencies={ "nvim-lua/plenary.nvim" }
 	}
 }
 require("lazy").setup(plugins)
 require("catppuccin").setup()
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+require("telescope").setup({
+	pickers = {
+		find_files = { theme = "cursor" }
+	}
+})
+-- live grep (C-f)
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
-vim.cmd.colorsheme "catppuccin"
+vim.cmd.colorscheme "catppuccin"
