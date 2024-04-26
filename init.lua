ae438609e689f61f44c8801c6d57b676427e2f37
23060107-Gong Kai Chen <1106029170@qq.com>
2024-04-26 17:04:35 +0800

    添加lazy包管理器

--- init.lua
+++ init.lua
@@ -3,3 +3,21 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+
+
+-- 添加包管理器 lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	print("hello", lazypath)
+	vim.fn.system({
+		"git", "clone", "--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+local plugins = {
+
+}
+require("lazy").setup(plugins)
