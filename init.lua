6a1153da9e3ccd203eaa554877a1407710597422
23060107-Gong Kai Chen <1106029170@qq.com>
2024-04-26 17:41:31 +0800

    添加文件管理树

--- init.lua
+++ init.lua
@@ -34,6 +34,16 @@ local plugins = {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build=":TSUpdate"
+	},
+	-- 添加 neo-tree 文件管理树
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch="v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim"
+		}
 	}
 }
 require("lazy").setup(plugins)
