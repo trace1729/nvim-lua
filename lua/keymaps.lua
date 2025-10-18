b5a5e8377842754c22a87aa34fe51c58dd017ec4
gongkch <1106029170@qq.com>
2025-10-18 12:35:48 +0800

    apply custom config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,95 +1,101 @@
 vim.g.mapleader = " "
-vim.g.maplocalleader = ','
 
 local mode_nv = { "n", "v" }
 local mode_v = { "v" }
 local mode_i = { "i" }
 local nmappings = {
-	{ from = "S",             to = ":w<CR>" },
-	{ from = "Q",             to = ":q<CR>" },
-	{ from = ";",             to = ":",                                                                   mode = mode_nv },
-	{ from = "Y",             to = "\"+y",                                                                mode = mode_v },
-	{ from = "`",             to = "~",                                                                   mode = mode_nv },
+	{ from = "S",                to = ":w<CR>" },
+	{ from = "Q",                to = ":q<CR>" },
+	{ from = ";",               to = ":",                                                                   mode = mode_nv },
+	{ from = "Y",                to = "\"+y",                                                                mode = mode_v },
+	{ from = "`",               to = "~",                                                                   mode = mode_nv },
 
 	-- Movement
-	{ from = "u",             to = "k",                                                                   mode = mode_nv },
-	{ from = "e",             to = "j",                                                                   mode = mode_nv },
-	{ from = "n",             to = "h",                                                                   mode = mode_nv },
-	{ from = "i",             to = "l",                                                                   mode = mode_nv },
-	{ from = "U",             to = "5k",                                                                  mode = mode_nv },
-	{ from = "E",             to = "5j",                                                                  mode = mode_nv },
-	{ from = "N",             to = "0",                                                                   mode = mode_nv },
-	{ from = "I",             to = "$",                                                                   mode = mode_nv },
-	{ from = "gu",            to = "gk",                                                                  mode = mode_nv },
-	{ from = "ge",            to = "gj",                                                                  mode = mode_nv },
-	{ from = "h",             to = "e",                                                                   mode = mode_nv },
-	{ from = "<C-U>",         to = "5<C-y>",                                                              mode = mode_nv },
-	{ from = "<C-E>",         to = "5<C-e>",                                                              mode = mode_nv },
-	{ from = "ci",            to = "cl", },
-	{ from = "cn",            to = "ch", },
-	{ from = "ck",            to = "ci", },
-	{ from = "c,.",           to = "c%", },
-	{ from = "yh",            to = "ye", },
+	{ from = "i",                to = "k",                                                                   mode = mode_nv },
+	{ from = "k",                to = "j",                                                                   mode = mode_nv },
+	{ from = "j",                to = "h",                                                                   mode = mode_nv },
+	{ from = "z",                to = "i",                                                                   mode = mode_nv },
+	{ from = "I",                to = "5k",                                                                  mode = mode_nv },
+	{ from = "K",                to = "5j",                                                                  mode = mode_nv },
+	{ from = "Z",                to = "I",                                                                   mode = mode_nv },
+	--	{ from = "N",               to = "0",                                                                   mode = mode_nv },
+	{ from = "E",                to = "$",                                                                   mode = mode_nv },
+	{ from = "gu",               to = "gk",                                                                  mode = mode_nv },
+	{ from = "ge",               to = "gj",                                                                  mode = mode_nv },
+	{ from = "h",                to = "e",                                                                   mode = mode_nv },
+	{ from = "<C-U>",            to = "5<C-y>",                                                              mode = mode_nv },
+	{ from = "<C-E>",            to = "5<C-e>",                                                              mode = mode_nv },
+	{ from = "c,.",              to = "c%", },
 
-	-- Actions
-	{ from = "l",             to = "u" },
-	{ from = "k",             to = "i",                                                                   mode = mode_nv },
-	{ from = "K",             to = "I",                                                                   mode = mode_nv },
 
 	-- Useful actions
-	{ from = ",.",            to = "%",                                                                   mode = mode_nv },
-	{ from = "<c-y>",         to = "<ESC>A {}<ESC>i<CR><ESC>ko",                                          mode = mode_i },
-	{ from = "\\v",           to = "v$h", },
-	{ from = "<c-a>",         to = "<ESC>A",                                                              mode = mode_i },
+	{ from = ",.",               to = "%",                                                                   mode = mode_nv },
+	{ from = "<c-y>",            to = "<ESC>A {}<ESC>i<CR><ESC>ko",                                          mode = mode_i },
+	{ from = "\\v",              to = "v$h", },
+	{ from = "<c-a>",            to = "<ESC>A",                                                              mode = mode_i },
 
 	-- Window & splits
-	{ from = "<leader>w",     to = "<C-w>w", },
-	{ from = "<leader>u",     to = "<C-w>k", },
-	{ from = "<leader>e",     to = "<C-w>j", },
-	{ from = "<leader>n",     to = "<C-w>h", },
-	{ from = "<leader>i",     to = "<C-w>l", },
-	{ from = "qf",            to = "<C-w>o", },
-	{ from = "s",             to = "<nop>", },
-	{ from = "su",            to = ":set nosplitbelow<CR>:split<CR>:set splitbelow<CR>", },
-	{ from = "se",            to = ":set splitbelow<CR>:split<CR>", },
-	{ from = "sn",            to = ":set nosplitright<CR>:vsplit<CR>:set splitright<CR>", },
-	{ from = "si",            to = ":set splitright<CR>:vsplit<CR>", },
-	{ from = "<up>",          to = ":res +5<CR>", },
-	{ from = "<down>",        to = ":res -5<CR>", },
-	{ from = "<left>",        to = ":vertical resize-5<CR>", },
-	{ from = "<right>",       to = ":vertical resize+5<CR>", },
+	{ from = "<leader>w",        to = "<C-w>w", },
+	{ from = "<leader>i",        to = "<C-w>k", },
+	{ from = "<leader>k",        to = "<C-w>j", },
+	{ from = "<leader>j",        to = "<C-w>h", },
+	{ from = "<leader>l",        to = "<C-w>l", },
+	{ from = "<leader><up>",     to = "<C-w>k", },
+	{ from = "<leader><down>",   to = "<C-w>j", },
+	{ from = "<leader><left>",   to = "<C-w>h", },
+	{ from = "<leader><right>",  to = "<C-w>l", },
+	{ from = "qf",               to = "<C-w>o", },
+	{ from = "s",                to = "<nop>", },
+	{ from = "J",                to = "<nop>", },
+	{ from = "su",               to = ":set nosplitbelow<CR>:split<CR>:set splitbelow<CR>", },
+	{ from = "sd",               to = ":set splitbelow<CR>:split<CR>", },
+	{ from = "sl",               to = ":set nosplitright<CR>:vsplit<CR>:set splitright<CR>", },
+	{ from = "sr",               to = ":set splitright<CR>:vsplit<CR>", },
+	{ from = "<leader>'<up>",    to = ":res +5<CR>", },
+	{ from = "<leader>'<down>",  to = ":res -5<CR>", },
+	{ from = "<leader>'<left>",  to = ":vertical resize-5<CR>", },
+	{ from = "<leader>'<right>", to = ":vertical resize+5<CR>", },
 	-- { from = "sh",            to = "se", },
 	-- { from = "sh",            to = "<C-w>t<C-w>K", },
 	-- { from = "sv",            to = "<C-w>t<C-w>H", },
-	{ from = "srh",           to = "<C-w>b<C-w>K", },
-	{ from = "srv",           to = "<C-w>b<C-w>H", },
+	{ from = "srh",              to = "<C-w>b<C-w>K", },
+	{ from = "srv",              to = "<C-w>b<C-w>H", },
 
 	-- Tab management
-	{ from = "tu",            to = ":tabe<CR>", },
-	{ from = "tU",            to = ":tab split<CR>", },
-	{ from = "tn",            to = ":-tabnext<CR>", },
-	{ from = "ti",            to = ":+tabnext<CR>", },
-	{ from = "tmn",           to = ":-tabmove<CR>", },
-	{ from = "tmi",           to = ":+tabmove<CR>", },
+	{ from = "tu",               to = ":tabe<CR>", },
+	{ from = "<leader>n",        to = ":tabe<CR>", },
+	{ from = "tU",               to = ":tab split<CR>", },
+	{ from = "th",               to = ":-tabnext<CR>", },
+	{ from = "tl",               to = ":+tabnext<CR>", },
+	{ from = "tmn",              to = ":-tabmove<CR>", },
+	{ from = "tmi",              to = ":+tabmove<CR>", },
 
 	-- Other
-	{ from = "<leader>sw",    to = ":set wrap<CR>" },
-	{ from = "<leader>sc",    to = ":set spell!<CR>" },
-	{ from = "<leader><CR>",  to = ":nohlsearch<CR>" },
-	{ from = "<f10>",         to = ":TSHighlightCapturesUnderCursor<CR>" },
-	{ from = "<leader>o",     to = "za" },
-	{ from = "<leader>pr",    to = ":profile start profile.log<CR>:profile func *<CR>:profile file *<CR>" },
-	{ from = "<leader>rc",    to = ":e ~/.config/nvim/init.lua<CR>" },
-	{ from = "<leader>rv",    to = ":e .vim.lua<CR>" },
-	{ from = ",v",            to = "v%" },
-	{ from = "<leader><esc>", to = "<nop>" },
+	{ from = "<leader>sw",       to = ":set wrap<CR>" },
+	{ from = "<leader>sc",       to = ":set spell!<CR>" },
+	{ from = "<leader><CR>",     to = ":nohlsearch<CR>" },
+	{ from = "<f10>",            to = ":TSHighlightCapturesUnderCursor<CR>" },
+	{ from = "<leader>o",        to = "za" },
+	{ from = "<leader>pr",       to = ":profile start profile.log<CR>:profile func *<CR>:profile file *<CR>" },
+	{ from = "<leader>rc",       to = ":e ~/.config/nvim/init.lua<CR>" },
+	{ from = "<leader>rv",       to = ":e .vim.lua<CR>" },
+	{ from = ",v",               to = "v%" },
+	{ from = "<leader><esc>",    to = "<nop>" },
 
 	-- Joshuto
-	{ from = "R",             to = ":Joshuto<CR>" },
+	{ from = "<leader>e",        to = ":Joshuto<CR>" },
+	{ from = "T",                to = ":terminal<CR>" },
+	{ from = "<leader>'e",       to = ":NvimTreeToggle<CR>" },
 }
 
 vim.keymap.set("n", "q", "<nop>", { noremap = true })
 vim.keymap.set("n", ",q", "q", { noremap = true })
+-- substitution
+vim.keymap.set("n", "\\s", ":%s+++g", { noremap = true })
+vim.keymap.set("n", "<leader>sp", ":setspell!<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>/", ":set splitbelow<CR>:split<CR>:res -5<CR>:term<CR>", { noremap = true })
+vim.keymap.set("n", "\\p", ":echo expand('%:p')<CR>", { noremap = true })
+
 
 for _, mapping in ipairs(nmappings) do
 	vim.keymap.set(mapping.mode or "n", mapping.from, mapping.to, { noremap = true })
@@ -101,10 +107,4 @@ local function run_vim_shortcut(shortcut)
 end
 
 -- close win below
-vim.keymap.set("n", "<leader>q", function()
-	require("trouble").close()
-	local wins = vim.api.nvim_tabpage_list_wins(0)
-	if #wins > 1 then
-		run_vim_shortcut([[<C-w>j:q<CR>]])
-	end
-end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>q", ":q<CR>")
--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -1,36 +1,36 @@
 return {
-	{
-		"kevinhwang91/nvim-hlslens",
-		lazy = false,
-		enabled = true,
-		keys = {
-			{
-				"=",
-				[[<cmd>execute('normal! ' . v:count1 . 'n')<cr>]] .. [[<cmd>lua require("hlslens").start()<cr>]],
-			},
-			{
-				"-",
-				[[<cmd>execute('normal! ' . v:count1 . 'N')<cr>]] .. [[<cmd>lua require("hlslens").start()<cr>]],
-			},
-			{ "*",  "*" .. [[<cmd>lua require("hlslens").start()<cr>]] },
-			{ "#",  "#" .. [[<cmd>lua require("hlslens").start()<cr>]] },
-			{ "g*", "g*" .. [[<cmd>lua require("hlslens").start()<cr>]] },
-			{ "g#", "g#" .. [[<cmd>lua require("hlslens").start()<cr>]] },
-		},
-		config = function()
-			require("scrollbar.handlers.search").setup()
-		end
-	},
-	{
-		"pechorin/any-jump.vim",
-		config = function()
-			vim.keymap.set("n", "j", ":AnyJump<CR>", { noremap = true })
-			vim.keymap.set("x", "j", ":AnyJumpVisual<CR>", { noremap = true })
-			vim.g.any_jump_disable_default_keybindings = true
-			vim.g.any_jump_window_width_ratio = 0.9
-			vim.g.any_jump_window_height_ratio = 0.9
-		end
-	},
+	-- {
+		-- "kevinhwang91/nvim-hlslens",
+		-- lazy = false,
+		-- enabled = true,
+		-- keys = {
+			-- {
+				-- "=",
+				-- [[<cmd>execute('normal! ' . v:count1 . 'n')<cr>]] .. [[<cmd>lua require("hlslens").start()<cr>]],
+			-- },
+			-- {
+				-- "-",
+				-- [[<cmd>execute('normal! ' . v:count1 . 'N')<cr>]] .. [[<cmd>lua require("hlslens").start()<cr>]],
+			-- },
+			-- { "*",  "*" .. [[<cmd>lua require("hlslens").start()<cr>]] },
+			-- { "#",  "#" .. [[<cmd>lua require("hlslens").start()<cr>]] },
+			-- { "g*", "g*" .. [[<cmd>lua require("hlslens").start()<cr>]] },
+			-- { "g#", "g#" .. [[<cmd>lua require("hlslens").start()<cr>]] },
+		-- },
+		-- config = function()
+			-- require("scrollbar.handlers.search").setup()
+		-- end
+	-- },
+	-- {
+		-- "pechorin/any-jump.vim",
+		-- config = function()
+			-- vim.keymap.set("n", "j", ":AnyJump<CR>", { noremap = true })
+			-- vim.keymap.set("x", "j", ":AnyJumpVisual<CR>", { noremap = true })
+			-- vim.g.any_jump_disable_default_keybindings = true
+			-- vim.g.any_jump_window_width_ratio = 0.9
+			-- vim.g.any_jump_window_height_ratio = 0.9
+		-- end
+	-- },
 	{
 		"MagicDuck/grug-far.nvim",
 		keys = {
